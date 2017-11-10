e672e5d001d3411d5812726c0242a9f2e9b4a66b
MoneyLordYT <33273499+MoneyLordYT@users.noreply.github.com>
2017-11-10 16:34:23 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '$newvid') {
+    	message.reply('Make sure to subscribe to MoneyLord if you have not already!\nLatest Upload: https://goo.gl/iktfbb\n\nDownload:\nIn the video's description\n```\nEnjoy the New Awesome Exploit Posted by one and the Only [MoneyLord]');
   	}
 });
 
